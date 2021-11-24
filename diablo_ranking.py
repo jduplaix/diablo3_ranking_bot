fd6cb888b049e0f66fd270bb305e8b3dc6790542
jduplaix <jonathan.duplaix@etudiant.univ-lr.fr>
2021-11-24 14:17:59 +0100

    feat: début parsing runs groupe + factorisation print run solo

--- diablo_ranking.py
+++ diablo_ranking.py
@@ -3,6 +3,25 @@ import json
 
 lboards = ['barbarian', 'monk','dh', 'wd', 'wizard','crusader', 'team-2']
 
+def format_solo_best(run):
+    print(f">> 1er: {run['player_data'][0]['btag']}\
+({run['player_data'][0].get('ctag','<Pas de clan>')}) \
+-> GR{run['rift_data']['grlvl']} \
+en {run['rift_data']['time']}")
+    print("\n\nTop BriTs :")
+
+def format_solo_rank(cr, run):
+    print(f"#{cr} : {run['player_data'][0]['btag']} \
+-> GR{run['rift_data']['grlvl']} \
+en {run['rift_data']['time']}")
+
+def format_team_best(run):
+    print(len(run['player_data']))
+    print(f"GR{run['rift_data']['grlvl']} en {run['rift_data']['time']}")
+
+def format_team_rank(run):
+    pass
+
 for lb in lboards:
     # Récupération de chaque flux classement sur maxroll.gg
     url =f'https://assets.maxroll.gg/leaderboards/s24-eu-rift-hardcore-{lb}.json'
@@ -14,26 +33,23 @@ for lb in lboards:
     clan_rank = 0
     # Print en-tête titre classement
     print(f"\n-------------------------\nClassement hardcore-{lb} :")
-    if "team" in lb:
-        # print à factoriser vers une méthode dédiée
-        for i in data:
-            for p in i['player_data']:
-                print(p)
-            break
-    else:
-        for i in data:
-            # print à factoriser vers une méthode dédiée
-            if i['rift_data']['api_rank'] == 1:
-                print(f">> 1er: {i['player_data'][0]['btag']}\
-({i['player_data'][0].get('ctag','<Pas de clan>')}) \
--> GR{i['rift_data']['grlvl']} \
-en {i['rift_data']['time']}\
-\n\nTop BriTs :")
-            elif 'ctag' in i['player_data'][0]:
-                if i['player_data'][0]['ctag'] == "BriT" and \
-                    i['player_data'][0]['cname'] == "BriTon" and \
+    for run in data:
+        if "team" in lb:
+            if run['rift_data']['api_rank'] == 1:
+                    format_team_best(run)
+            # Print à factoriser vers une méthode dédiée
+            #for run in data:
+            #    for players in run['player_data']:
+            #        print(players)
+            #    break
+        else:
+            # Print à factoriser vers une méthode dédiée
+            if run['rift_data']['api_rank'] == 1:
+                format_solo_best(run)
+            elif 'ctag' in run['player_data'][0]:
+                if run['player_data'][0]['ctag'] == "BriT" and \
+                    run['player_data'][0]['cname'] == "BriTon" and \
                     clan_rank < 5:
                     clan_rank += 1
-                    print(f"#{clan_rank} : {i['player_data'][0]['btag']} \
--> GR{i['rift_data']['grlvl']} \
-en {i['rift_data']['time']}")
+                    format_solo_rank(clan_rank, run)
+
