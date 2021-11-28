205c2e3f3017fd7cb12b6360825762a86960c6d0
jduplaix <jonathan.duplaix@etudiant.univ-lr.fr>
2021-11-28 12:22:55 +0100

    feat: styling en-tête jaune + btag&grlvl bold

--- maxroll_scrap.py
+++ maxroll_scrap.py
@@ -16,17 +16,19 @@ def get_single_class(lb, season = str(get_current_season()), mode = "hardcore"):
     json_data = json.loads(response.content)
     data = json_data['data']
     clan_rank = 0
-    best_run = f"(1er EU: GR{data[0]['rift_data']['grlvl']} en {data[0]['rift_data']['time']})"
-    res = f"**>> CLASSEMENT {mode.upper()} {lb.upper()} S{season} **{best_run}** <<**\n"
+    best_run = f" 1er EU: GR{data[0]['rift_data']['grlvl']} en {data[0]['rift_data']['time']}"
+    #```fix ``` = coloration jaune du §
+    res = f"```fix\n>> CLASSEMENT {mode.upper()} {lb.upper()} S{season} <<\n"
+    res = res + f"> {best_run}\n```"
     for run in data:
         if 'ctag' in run['player_data'][0]:
             if run['player_data'][0]['ctag'] == "BriT" and \
             run['player_data'][0]['cname'] == "BriTon" and \
             clan_rank < 5:
                 clan_rank += 1
-                res = res + f"\n#{clan_rank} : {run['player_data'][0]['btag']} \
+                res = res + f"\n#{clan_rank} : **{run['player_data'][0]['btag']}** \
 p{run['player_data'][0]['plvl']} \
--> GR{run['rift_data']['grlvl']} \
+-> **GR{run['rift_data']['grlvl']}** \
 en {run['rift_data']['time']}"
     return res
 
@@ -37,4 +39,14 @@ def get_teams(lb, season = str(get_current_season()), mode = "hardcore"):
     response = requests.get(url)
     json_data = json.loads(response.content)
     data = json_data['data']
-    return f"<{lb} S{get_current_season()} *prochainement disponible*>"
+
+    # Parsing du #1er run EU
+    best_classes = ""
+    for player in data[0]['player_data']:
+        best_classes = best_classes + f"[{player['class']}]"
+    best_run = f" 1ers EU: {best_classes} GR{data[0]['rift_data']['grlvl']} en {data[0]['rift_data']['time']}"
+    #```fix ``` = coloration jaune du §
+    res = "```fix\n"
+    res = res + f">> CLASSEMENT {mode.upper()} {lb.upper()} S{season} <<\n"
+    res = res + f"> {best_run}\n```"
+    return res + "\n*Top BriTs prochainement*"
