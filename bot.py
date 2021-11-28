a2dae3d97c187450f9a84a70269fd2e9ccf2ea0d
jduplaix <jonathan.duplaix@etudiant.univ-lr.fr>
2021-11-28 16:13:04 +0100

    feat: identification des options classements BriTs + factorisation envoi réponse

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import maxroll_scrap
+import re
 
 # TODO : Valorisation du token du bot à adapter en fonction de l'hébergement
 token = open(".env", "r")
@@ -19,32 +20,48 @@ async def on_ready():
 # Fonction d'écoute de mot clef
 @client.event
 async def on_message(message):
-    print(f"Commande reçue : '{message.content}'")
+    print(f"Message reçu : '{message.content}'")
     cmd = message.content
+    # Si le message posté ne commence pas par un '!' c'est pas une commande
     if cmd[0] == "!":
         cmd = cmd[1:]
 
-    # Classement saison en cours /lboard
-    if cmd in lboards:
-        if 'team' in cmd:
-            r = maxroll_scrap.get_teams(cmd)
-        else:
-            r = maxroll_scrap.get_single_class(cmd)
-        await message.channel.send(r)
+        ### Commandes classements BriTs
+        # vérif présence d'options
+        if " " in cmd:
+            cmd = cmd.split(" ")
+            # contrôle de validité du radical de la commande
+            if cmd[0] in lboards:
+                # identification des options
+                for arg in cmd:
+                    if arg == "--soft":
+                        mode = arg
+                    # si saisie multiple d'option num saison, seule la première est lue 
+                    if re.search("^--\d\d$", arg) and not 'season' in locals():
+                        season = re.sub('-', '', arg)
+                # valorisation des appels scrap en fonction des options trouvées
 
-    # Liste des commandes
-    if cmd == "help":
-        r = f"**Usage : classements BriTs**\n"
-        r = r + f"```" + "!classement --saison --soft" + "```"
-        r = r + f"__Classements__ : {str(lboards)}\n"
-        r = r + f"\n **Option** *--saison* : s1 à s{str(maxroll_scrap.get_current_season())}. Facultatif, saison en cours par défaut."
-        r = r + f"\n **Option** *--soft* : retourne les classements softcore. Facultatif, classements hardcore par défaut.\n"
-        r = r + f"Exemples :"
-        r = r + f"```!dh```"
-        r = r + f"```!team-4```"
-        r = r + f"```!crusader --s13```"
-        r = r + "\n**Usage : classements par BattleTag**\n<*Coming soon*>"
-        await message.channel.send(r)
+        # Si un seul param, alors uniquement type ladder (sinon cmd invalide sans feedback user)
+        else:
+            if cmd in lboards:
+                if 'team' in cmd:
+                    r = maxroll_scrap.get_teams(cmd)
+                else:
+                    r = maxroll_scrap.get_single_class(cmd)
+        
+        # Liste des commandes
+        if cmd == "help":
+            r = f"**Usage : classements BriTs**\n"
+            r = r + f"```" + "!classement --saison --soft" + "```"
+            r = r + f"__Classements__ : {str(lboards)}\n"
+            r = r + f"\n **Option** *--numéro de saison* : 1 à {str(maxroll_scrap.get_current_season())}. Facultatif, saison en cours par défaut."
+            r = r + f"\n **Option** *--soft* : retourne les classements softcore. Facultatif, classements hardcore par défaut.\n"
+            r = r + f"Exemples :"
+            r = r + f"```!dh```"
+            r = r + f"```!team-4```"
+            r = r + f"```!crusader --13```"
+            r = r + "\n**Usage : classements par BattleTag**\n<*Coming soon*>"
+    if r : await message.channel.send(r)
 
 # Exécution du bot
 client.run(token)
